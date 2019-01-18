abcbcd01995e65851b2ff1f74e1ed8b7adc9e608
nuno837 <nunofilmaia@yahoo.com.br>
2019-01-18 17:24:44 +0000

    Inicialização do projeto
    Criação do menu inicial de login (com um sistema de login estatico)
    Criação do layout de sign in
    Criação do layout "pós login" (para verificar se o login for efetuado)

---
+++ app/src/main/java/pt/ipp/estg/cmu_tp/MainActivity.java
@@ -0,0 +1,65 @@
+package pt.ipp.estg.cmu_tp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private Button SignIn;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = findViewById(R.id.etName);
+        Password = findViewById(R.id.etPassword);
+        Info = findViewById(R.id.tvInfo);
+        Login = findViewById(R.id.loginButton);
+        SignIn = findViewById(R.id.signInButton);
+
+        Info.setText("Nr of incorrect attempts: 0");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))){
+
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter++;
+
+            Info.setText("Nr of incorrect attempts:" + String.valueOf(counter));
+
+            if(counter == 10){
+                Login.setEnabled(false);
+            }
+        }
+    }
+}
